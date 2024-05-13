e2a68532eab33900fabb12c7dd4b9277544bac0f
adegbite mohammed <adedamola13@gmail.com>
2024-05-13 15:02:34 +0100

    Adding this to my existing folder

---
+++ day seven/menstrualWahala/MenstrualCycleCalculator.java
@@ -0,0 +1,49 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+
+public class MenstrualCycleCalculator{
+
+	public static void main(String[] args){
+
+	Scanner scanner = new Scanner(System.in);
+
+	LocalDate currentDate = LocalDate.now();
+	
+	System.out.print("WELCOME TO THE MENSTRUAL CYCLE CALCULATOR");
+	
+	System.out.print("\nAssumed Length Of Cycle Is Between 21 - 35 Days");
+
+	System.out.print("\nEnter The Average Length Of Your Cycle: ");
+
+	String cycleLength = scanner.nextLine();
+
+	System.out.print("\nEnter The Duration Of Menstrual Flow In Days: ");
+
+	String flowDuration = scanner.nextLine();
+
+	System.out.print("\nEnter The First Day Of Current Menstrual Cycle: ");
+
+	String startDate = scanner.nextLine();
+
+	
+
+
+
+
+	
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+}
