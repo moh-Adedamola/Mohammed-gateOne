1e4fd947161ba395d4f7fa6ce83ff311ccef56b1
adegbite mohammed <adedamola13@gmail.com>
2024-05-15 03:02:53 +0100

    Adding this to my existing folder

---
+++ day seven/menstrualWahala/Menstrualcyclecalculator.js
@@ -0,0 +1,6 @@
+console.log("WELCOME TO THE MENSTRUAL CYCLE CALCULATOR");
+
+console.log("Assumed Length Of Cycle Is Between 21 - 35 Days");
+
+const lastFlowDate = prompt("Enter The Date Of Your Last Menstrual Flow(YYYY-MM-DD): ");
+
