a6d403954b3d8ee6e5a983aaf8d68f525f19ccf1
adegbite mohammed <adedamola13@gmail.com>
2024-05-15 01:12:10 +0100

    Adding this to my existing folder

--- day seven/menstrualWahala/menstrualcyclecalculator.py
+++ day seven/menstrualWahala/menstrualcyclecalculator.py
@@ -1,27 +1,27 @@
 from datetime import datetime
 
-def calculate_cycle(menstrual_cycle_calculator):
 
-	print("WELCOME TO THE MENSTRUAL CYCLE CALCULATOR")
 
-	print("Assumed Length Of Cycle Is Between 21 - 35 Days")
+print("WELCOME TO THE MENSTRUAL CYCLE CALCULATOR")
 
-	last_flow_date = input("Enter The Date Of Your Last Menstrual Flow(YYYY-MM-DD): ")
+print("Assumed Length Of Cycle Is Between 21 - 35 Days")
 
-	cycle_length = int(input("Enter The Average Length Of Your Cycle In Days: "))
+last_flow_date = input("Enter The Date Of Your Last Menstrual Flow(YYYY-MM-DD): ")
 
-	next_menstrual_flow = last_flow_date + cycle_length
+cycle_length = int(input("Enter The Average Length Of Your Cycle In Days: "))
 
-	print("Your Next Menstrual Flow Starts On: ", next_menstrual_flow)
+next_menstrual_flow = last_flow_date + cycle_length
 
-	ovulation_date = next_menstrual_flow - 14
+print("Your Next Menstrual Flow Starts On: ", next_menstrual_flow)
 
-	print("Your Ovulation Date Is: ", ovulation_date)
+ovulation_date = next_menstrual_flow - 14
 
-	fertile_period_start = ovulation_date - 7
+print("Your Ovulation Date Is: ", ovulation_date)
 
-	fertile_period_end = ovulation_date + 2
+fertile_period_start = ovulation_date - 7
 
-	print("Your Fertile Period Is Between: ", fertile_period_start, "-" , fertile_period_end)
+fertile_period_end = ovulation_date + 2
 
-	print("Your Safe Period Is Between: ", safe_period_start, "-" , safe_period_end  )
+print("Your Fertile Period Is Between: ", fertile_period_start, "-" , fertile_period_end)
+
+print("Your Safe Period Is Between: ", safe_period_start, "-" , safe_period_end  )
